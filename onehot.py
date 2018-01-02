cfc81684f4e493d0237667f17df194225a3815c4
steven0129 <h3584935@gmail.com>
2018-01-03 00:27:28 +0800

    Add onehot encoder into this project

---
+++ onehot.py
@@ -0,0 +1,30 @@
+import pickle
+
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+# define example
+dataset = []
+for i in range(0, 195102):
+    try:
+        word = chr(i)
+        dataset.append(word)
+    except:
+        pass
+
+# integer encode
+label_encoder = None
+with open('label.pickle', 'rb') as f:
+    label_encoder = pickle.load(f)
+integer_encoded = label_encoder.fit_transform(dataset)
+print(integer_encoded)
+# binary encode
+onehot_encoder = OneHotEncoder()
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoder.fit(integer_encoded)
+
+with open('onehot.pickle', 'wb') as f:
+    pickle.dump(onehot_encoder, f)
+
+print('目前收錄: ' + str(len(dataset)) + '個字')
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import pickle
 
-test = ['我', '愛', '高', '\\n']
+test = ['我', '愛', '高']
+integer_encode = None
 
 with open('label.pickle', 'rb') as f:
     label = pickle.load(f)
     print(label.transform(test))
+    integer_encode = label.transform(test)
+
+with open('onehot.pickle', 'rb') as f:
+    label = pickle.load(f)
+    print(label.transform(integer_encode.reshape(len(integer_encode), 1)))
